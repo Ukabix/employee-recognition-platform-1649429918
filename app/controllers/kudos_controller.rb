8dad209c2858f514e52b6868bc728921f3c6104b
Lukasz P <lukasz.piliszek@gmail.com>
2022-04-10 18:08:51 +0200

    set kudo index as root, set redirect on kudo post, remove double alerts from kudo/index, set giver and receiver emails instead of ids

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -25,7 +25,7 @@ class KudosController < ApplicationController
     @kudo.giver = current_employee
 
     if @kudo.save
-      redirect_to @kudo, notice: 'Kudo was successfully created.'
+      redirect_to kudos_path, notice: 'Kudo was successfully created.'
     else
       render :new
     end
